c373d4e60d2c484db26b09193f95165d49eee9d0
Maud <marieaude.torchy2022@campus-eni.fr>
2023-04-27 16:24:36 +0200

    Debut accueil sur twig

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\SortieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,9 +11,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class MainController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function home(): Response
+    public function home(SortieRepository $sortieRepository): Response
     {
+        $sortie = $sortieRepository->findAll();
+
         return $this->render('main/home.html.twig', [
+            "sorties"=>$sortie,
 
         ]);
     }
