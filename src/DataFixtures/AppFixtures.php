857a3873687a31504c742ad485915854a7d0d942
Maud <marieaude.torchy2022@campus-eni.fr>
2023-04-28 16:50:42 +0200

    Création fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -84,109 +84,111 @@ class AppFixtures extends Fixture
             $manager->persist($lieu);
         }
 
-        //--------Etat----------
-        $etats = ['Créée', 'Ouverte', 'Clôturée', 'Activité en cours', 'Passée', 'Annulée'];
-        foreach ($etats as $libelle){
-            $etat = new Etat();
-            $etat->setLibelle($libelle);
-            $manager->persist($etat);
-        }
+                //--------Etat----------
+                $etats = ['Créée', 'Ouverte', 'Clôturée', 'Activité en cours', 'Passée', 'Annulée'];
+                foreach ($etats as $libelle){
+                    $etat = new Etat();
+                    $etat->setLibelle($libelle);
+                    $manager->persist($etat);
+                }
 
-        //--------Campus----------
-        $campusArray = ['Nantes', 'Rennes', 'Quimper', 'Niort'];
-        foreach ($campusArray as $campusName){
-            $campus = new Campus();
-            $campus->setNom($campusName);
-            $manager->persist($campus);
-        }
+                        //--------Campus----------
+                        $campusArray = ['Nantes', 'Rennes', 'Quimper', 'Niort'];
+                        foreach ($campusArray as $campusName){
+                            $campus = new Campus();
+                            $campus->setNom($campusName);
+                            $manager->persist($campus);
+                        }
 
-        //--------User----------
-        $campus = $this->campusRepository->findAll();
-        $user1 = new User();
-        $user1->setEmail('test@test.com');
-        $user1->setRoles(['ROLE_USER']);
+                               //--------User----------
+                               $campus = $this->campusRepository->findAll();
+                               $user1 = new User();
+                               $user1->setEmail('test@test.com');
+                               $user1->setRoles(['ROLE_USER']);
 
-        $password1 = $this->hasher->hashPassword($user1, 'pass_1234');
-        $user1->setPassword($password1);
+                               $password1 = $this->hasher->hashPassword($user1, 'pass_1234');
+                               $user1->setPassword($password1);
 
-        $user1->setNom('Test');
-        $user1->setPrenom('SuperTest');
-        $user1->setTelephone('0652369485');
-        $user1->setActif(true);
-        $user1->setCampus($faker->randomElement($campus));
-        $user1->setPseudo('SupTest');
-        $manager->persist($user1);
-
-
-        $user2 = new User();
-        $user2->setEmail('pif@test.com');
-        $user2->setRoles(['ROLE_USER']);
-
-        $password2 = $this->hasher->hashPassword($user2, 'pass_1234');
-        $user2->setPassword($password2);
-
-        $user2->setNom('Paf');
-        $user2->setPrenom('Pouf');
-        $user2->setTelephone('0652369485');
-        $user2->setActif(true);
-        $user2->setCampus($faker->randomElement($campus));
-        $user2->setPseudo('PifPafPouf');
-        $manager->persist($user2);
+                               $user1->setNom('Test');
+                               $user1->setPrenom('SuperTest');
+                               $user1->setTelephone('0652369485');
+                               $user1->setActif(true);
+                               $user1->setCampus($faker->randomElement($campus));
+                               $user1->setPseudo('SupTest');
+                               $manager->persist($user1);
 
 
-        $admin = new User();
-        $admin->setEmail('admin@test.com');
-        $admin->setRoles(['ROLE_ADMIN']);
+                               $user2 = new User();
+                               $user2->setEmail('pif@test.com');
+                               $user2->setRoles(['ROLE_USER']);
 
-        $password3 = $this->hasher->hashPassword($admin, 'pass_1234');
-        $admin->setPassword($password3);
+                               $password2 = $this->hasher->hashPassword($user2, 'pass_1234');
+                               $user2->setPassword($password2);
 
-        $admin->setNom('Admin');
-        $admin->setPrenom('Istrateur');
-        $admin->setTelephone('0652369485');
-        $admin->setActif(true);
-        $admin->setCampus($faker->randomElement($campus));
-        $admin->setPseudo('Admin');
-        $manager->persist($admin);
-
-        //générer User aléatoire
-        $campus = $this->campusRepository->findAll();
-        for($i = 1; $i<=20; $i++){
-            $user = new User();
-            $user->setNom($faker->lastName);
-            $user->setPrenom($faker->firstName);
-            $user->setPseudo($faker->unique()->userName);
-            $user->setTelephone($faker->numerify('06########'));
-            $user->setEmail($faker->email);
-            $user->setPassword($this->hasher->hashPassword($user, 'pass_1234'));
-            $user->setRoles(['ROLE_USER']);
-            $user->setActif(true);
-            $user->setCampus($faker->randomElement($campus));
-            $manager->persist($user);
-        }
+                               $user2->setNom('Paf');
+                               $user2->setPrenom('Pouf');
+                               $user2->setTelephone('0652369485');
+                               $user2->setActif(true);
+                               $user2->setCampus($faker->randomElement($campus));
+                               $user2->setPseudo('PifPafPouf');
+                               $manager->persist($user2);
 
 
-        //--------Sortie----------
+                               $admin = new User();
+                               $admin->setEmail('admin@test.com');
+                               $admin->setRoles(['ROLE_ADMIN']);
 
-        //Génère Sortie Aléatoire
-        $organisateurS = $this->userRepository->findAll();
-        $lieuS = $this->lieuRepository->findAll();
-        $etatS = $this->etatRepository->findAll();
-        for ($i = 1; $i<=50; $i++){
-            $sortie = new Sortie();
-            $sortie->setNom($faker->sentence($nbWords =4, $variableNbWords = true));
-            $sortie->setDateHeureDebut($faker->dateTimeBetween('+1 days', '+1 month', 'Europe/Paris'));
-            $sortie->setDuree($faker->dateTimeBetween($sortie->getDateHeureDebut(), $endDate = '+1 days'));
-            $sortie->setDateLimiteInscription($faker->dateTimeBetween('-1 days', $sortie->getDateHeureDebut(), 'Europe/Paris'));
-            $sortie->setNbInscriptionsMax($faker->numberBetween($min = 3, $max = 25));
-            $sortie->setInfosSortie($faker->paragraph($nbSentences = 5, $variableNbSentences = true));
-            $sortie->setOrganisateur($faker->randomElement($organisateurS));
-            $sortie->setCampus($sortie->getOrganisateur()->getCampus());
-            $sortie->setLieu($faker->randomElement($lieuS));
-            $sortie->setEtat($faker->randomElement($etatS));
-            $manager->persist($sortie);
+                               $password3 = $this->hasher->hashPassword($admin, 'pass_1234');
+                               $admin->setPassword($password3);
 
-        }
+                               $admin->setNom('Admin');
+                               $admin->setPrenom('Istrateur');
+                               $admin->setTelephone('0652369485');
+                               $admin->setActif(true);
+                               $admin->setCampus($faker->randomElement($campus));
+                               $admin->setPseudo('Admin');
+                               $manager->persist($admin);
+
+                               //générer User aléatoire
+                               $campus = $this->campusRepository->findAll();
+                               for($i = 1; $i<=20; $i++){
+                                   $user = new User();
+                                   $user->setNom($faker->lastName);
+                                   $user->setPrenom($faker->firstName);
+                                   $user->setPseudo($faker->unique()->userName);
+                                   $user->setTelephone($faker->numerify('06########'));
+                                   $user->setEmail($faker->email);
+                                   $user->setPassword($this->hasher->hashPassword($user, 'pass_1234'));
+                                   $user->setRoles(['ROLE_USER']);
+                                   $user->setActif(true);
+                                   $user->setCampus($faker->randomElement($campus));
+                                   $manager->persist($user);
+                               }
+
+
+                                       //--------Sortie----------
+
+                                       //Génère Sortie Aléatoire
+                                       $organisateurS = $this->userRepository->findAll();
+                                       $lieuS = $this->lieuRepository->findAll();
+                                       $etatS = $this->etatRepository->findAll();
+                                       $user = $this->userRepository->findAll();
+                                       for ($i = 1; $i<=50; $i++){
+                                           $sortie = new Sortie();
+                                           $sortie->setNom($faker->sentence($nbWords =4, $variableNbWords = true));
+                                           $sortie->setDateHeureDebut($faker->dateTimeBetween('+1 days', '+1 month', 'Europe/Paris'));
+                                           $sortie->setDuree($faker->dateTimeBetween($sortie->getDateHeureDebut(), $endDate = '+1 days'));
+                                           $sortie->setDateLimiteInscription($faker->dateTimeBetween('-1 days', $sortie->getDateHeureDebut(), 'Europe/Paris'));
+                                           $sortie->setNbInscriptionsMax($faker->numberBetween($min = 3, $max = 25));
+                                           $sortie->setInfosSortie($faker->paragraph($nbSentences = 5, $variableNbSentences = true));
+                                           $sortie->setOrganisateur($faker->randomElement($organisateurS));
+                                           $sortie->setCampus($sortie->getOrganisateur()->getCampus());
+                                           $sortie->setLieu($faker->randomElement($lieuS));
+                                           $sortie->setEtat($faker->randomElement($etatS));
+                                           $sortie->addParticipant($faker->randomElement($user));
+                                           $manager->persist($sortie);
+
+                                       }
 
         $manager->flush();
     }
